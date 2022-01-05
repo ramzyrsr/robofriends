13ac2d04f86e1bbea0dd5b551633b7f75f7f48f7
Ramzy Syahrul <ramzy@kozii.id>
2022-01-05 10:07:23 +0700

    adding error condition

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './card';
 
 const CardList = ({ robots }) => {
+    if (false) {
+        throw new Error('Nooooooo!');
+    }
     return (
         <div>
             {
